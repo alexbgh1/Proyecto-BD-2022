b6f901e1316425a06da3525b1d47145d4ec2866c
Cristian Castro <89940786+Cristhian17xd@users.noreply.github.com>
2022-06-15 18:59:15 -0400

    Add files via upload
    create_db.py

--- create_db.py
+++ create_db.py
@@ -6,14 +6,44 @@ conn = mysql.connector.connect(
   password="root",
   port=3306
 )
-
 cur = conn.cursor()
-#crea una base de datos en mariadb
-cur.execute("CREATE DATABASE noticias_region_aysen")
-#se para en la base de datos creada
-cur.execute("USE noticias_region_aysen")
 
-#crea las tablas (medios de comunicacion)
+def creaData(nombreData):
+  cur.execute("CREATE DATABASE "+nombreData)
+  listo = "Base de datos creada con exito"
+  return listo
+
+def creacion(nombreDataBase,nombreTabla):
+  cur.execute("USE "+nombreDataBase)
+  cur.execute("CREATE TABLE "+ nombreTabla+" (id INT AUTO_INCREMENT PRIMARY KEY, url VARCHAR(255), titulo VARCHAR(200), autor VARCHAR(50),fechaDePublicacion DATE, contenidoPublicacion TEXT)")
+  listo = "tabla creada!"
+  return listo
+
+nombreBase = "netflix"
+listaTablas = ["peliculas","videos","musica","juegos","ancianos"]
+creaData(nombreBase)
+for i in listaTablas:
+  creacion(nombreBase,i)
+
+
+cur.execute("SHOW TABLES")
+for row in cur:
+  print(row)
+
+
+
+
+
+
+
+
+
+
+
+
+"""""
+cur.execute("CREATE DATABASE noticias_region_aysen")
+cur.execute("USE noticias_region_aysen")
 cur.execute("CREATE TABLE EPD_noticias (id INT AUTO_INCREMENT PRIMARY KEY, url VARCHAR(255), titulo VARCHAR(200), autor VARCHAR(50),fechaDePublicacion DATE, contenidoPublicacion TEXT)")
 cur.execute("CREATE TABLE Viento_patagon (id INT AUTO_INCREMENT PRIMARY KEY, url VARCHAR(255), titulo VARCHAR(200), autor VARCHAR(50), fechaDePublicacion DATE, contenidoPublicacion TEXT)")
 cur.execute("CREATE TABLE El_divisadero (id INT AUTO_INCREMENT PRIMARY KEY, url VARCHAR(255), titulo VARCHAR(200), autor VARCHAR(50), fechaDePublicacion DATE, contenidoPublicacion TEXT)")
@@ -25,10 +55,8 @@ cur.execute("CREATE TABLE Tehuelche_noticias (id INT AUTO_INCREMENT PRIMARY KEY,
 cur.execute("CREATE TABLE Canal_sur_patagonia (id INT AUTO_INCREMENT PRIMARY KEY, url VARCHAR(2550), titulo VARCHAR(200), autor VARCHAR(50),fechaDePublicacion DATE, contenidoPublicacion TEXT)")
 cur.execute("CREATE TABLE Risco_aysen (id INT AUTO_INCREMENT PRIMARY KEY, url VARCHAR(255), titulo VARCHAR(200), autor VARCHAR(50),fechaDePublicacion DATE, contenidoPublicacion TEXT)")
 cur.execute("CREATE TABLE Radio_las_nieves_FM (id INT AUTO_INCREMENT PRIMARY KEY, url VARCHAR(255), titulo VARCHAR(200), autor VARCHAR(50),fechaDePublicacion DATE, contenidoPublicacion TEXT)")
+"""
 
-#solo sirve para ver que se crearon las tablas
-cur.execute("SHOW TABLES")
-for row in cur:
-  print(row)
+
 
 
