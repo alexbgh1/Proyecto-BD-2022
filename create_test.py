152e5948ee9ce9b3aa42d5ba1bda32edfc2687d7
Cristian Castro <89940786+Cristhian17xd@users.noreply.github.com>
2022-06-28 17:04:07 -0400

    create_test.py
    fiel a modelo relacional

--- create_test.py
+++ create_test.py
@@ -5,7 +5,7 @@ import sys
 try:
     conn = mariadb.connect(
         user="root",
-        password="password", #root
+        password="root", #root
         host="127.0.0.1",
         port=3306
     )
@@ -26,21 +26,23 @@ def createDatabase():
 def createTables():
     cur.execute("USE Sophia3")
     #id INT AUTO_INCREMENT PRIMARY KEY, url VARCHAR(255), titulo VARCHAR(200), autor VARCHAR(50),fechaDePublicacion DATE, contenidoPublicacion TEXT
-    cur.execute("CREATE TABLE Person (person_id INT AUTO_INCREMENT PRIMARY KEY, profesion VARCHAR(100), fecha_nac DATE, name VARCHAR(100), wikipedia BOOL, wikipedia_url VARCHAR(250))") #Persona PK
-    cur.execute("CREATE TABLE News_outlet (news_outlet_url VARCHAR(250) PRIMARY KEY, name VARCHAR(100), creation_date DATE, region INT, country VARCHAR(50), language VARCHAR(50))") #Medios PK
-    cur.execute("CREATE TABLE Owner (owner_id VARCHAR(250) PRIMARY KEY, name VARCHAR(100), is_person BOOL)") #Dueño PK
+    cur.execute("CREATE TABLE Persona(persona INT AUTO_INCREMENT NOT NULL PRIMARY KEY, profesion VARCHAR(100), fecha_nac DATE, name VARCHAR(100), wikipedia BOOL, wikipedia_url VARCHAR(250))") #Persona PK
+
+    cur.execute("CREATE TABLE Score (puntaje INT PRIMARY KEY, persona INT REFERENCES Persona(persona), fechaPuntaje DATE, value INT)") #Puntaje FK (Persona)
+
+    cur.execute("CREATE TABLE Medio(url_medio VARCHAR(250) NOT NULL PRIMARY KEY, name VARCHAR(100), creation_date DATE, region INT, country VARCHAR(50), language VARCHAR(50))") #Medios PK    
+
+    cur.execute("CREATE TABLE Noticias(noticia_url VARCHAR(250) NOT NULL PRIMARY KEY, author_noticia VARCHAR(100), title_noticia VARCHAR(100), text_noticia TEXT, fecha_noticia DATE, url_medio VARCHAR(250) REFERENCES Medio(url_medio))") #Noticia FK (Medios)
+
+    cur.execute("CREATE TABLE Dueno(dueno VARCHAR(250) NOT NULL PRIMARY KEY, name VARCHAR(100), is_person BOOL)") #Dueño PK
+
+    cur.execute("CREATE TABLE Menciones(mencionar VARCHAR(200) PRIMARY KEY, noticia VARCHAR(250) NOT NULL, persona INT NOT NULL, FOREIGN KEY(noticia) REFERENCES Noticias(noticia_url), FOREIGN KEY(persona) REFERENCES Persona(persona))") #Menciones FK (Noticia y Persona)
+
+    cur.execute("CREATE TABLE Posee(posee VARCHAR(250) PRIMARY KEY, dueno VARCHAR(250) NOT NULL, url_medio VARCHAR(250) NOT NULL, fecha_inicio DATE, fecha_fin DATE, FOREIGN KEY(dueno) REFERENCES Dueno(dueno), FOREIGN KEY(url_medio) REFERENCES Medio(url_medio))") #Posee FK (Medios y Dueños)
+
+
+
     
-    #### LA LINEA QUE SIGUE ESTÁ MAL
-    # No supe poner las dos Foreign Key como PK (quizás se tenga que cambiar también el tipo de las otras pk para que coincida)
-    ##cur.execute("CREATE TABLE Owns (owns_id VARCHAR(250) PRIMARY KEY, FOREIGN KEY (owns_id) REFERENCES News_outlet(news_outlet_url), REFERENCES (owns_id) Owner(owner_id), fecha_inicio DATE, fecha_fin DATE)") #Posee FK (Medios y Dueños)
-    
-
-    cur.execute("CREATE TABLE New (new_url VARCHAR(250) PRIMARY KEY, FOREIGN KEY (new_url) REFERENCES News_outlet(news_outlet_url), author_new VARCHAR(100), title_new VARCHAR(100), text_new TEXT, fecha_new DATE)") #Noticia FK (Medios)
-    #### LA LINEA QUE SIGUE ESTÁ MAL
-    ##cur.execute("CREATE TABLE Mention (mention_id PRIMARY KEY , FOREIGN KEY (mention_id) REFERENCES New(mention_id),FOREIGN KEY (new_url) REFERENCES Person(person_id) )") #Menciones FK (Noticia y Persona)
-
-
-    cur.execute("CREATE TABLE Score (score_id INT PRIMARY KEY, FOREIGN KEY (score_id) REFERENCES Person(person_id), fecha_score DATE, value INT)") #Puntaje FK (Persona)
 
 createDatabase()
 createTables()
