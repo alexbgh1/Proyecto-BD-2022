ff0dfb62f6baa245b7592934e5114930ff6de2d9
Alex <alex.garnica@alumnos.uach.cl>
2022-07-01 21:23:09 -0400

    insert_manual.py, request.py - error en insertmenciones/posee

--- create_db.py
+++ create_db.py
@@ -1,38 +1,52 @@
-import mysql.connector
+import mariadb
+import sys
 
-conn = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  password="password", #root
-  port=3306
-)
+# Connect to MariaDB Platform
+try:
+    conn = mariadb.connect(
+        user="root",
+        password="password", #root
+        host="127.0.0.1",
+        port=3306
+    )
+
+except mariadb.Error as e:
+    print(f"Error connecting to MariaDB Platform: {e}")
+    sys.exit(1)
+
+# Get Cursor
 cur = conn.cursor()
 
-def creaData(nombreData):
-  cur.execute("CREATE DATABASE "+nombreData)
-  listo = "Base de datos creada con exito"
-  return listo
+# Create Database
+def createDatabase():
+    query_create = "CREATE DATABASE Sophia3"
+    cur.execute(query_create)
 
-def creacion(nombreDataBase,nombreTabla):
-  cur.execute("USE "+nombreDataBase)
-  cur.execute("CREATE TABLE "+ nombreTabla+" (id INT AUTO_INCREMENT PRIMARY KEY, url VARCHAR(255), titulo VARCHAR(200), autor VARCHAR(50),fechaDePublicacion DATE, contenidoPublicacion TEXT)")
-  listo = "tabla creada!"
-  return listo
+# Create tables (modelo relacional)
+def createTables():
+    cur.execute("USE Sophia3")
+    #id INT AUTO_INCREMENT PRIMARY KEY, url VARCHAR(255), titulo VARCHAR(200), autor VARCHAR(50),fechaDePublicacion DATE, contenidoPublicacion TEXT
+    cur.execute("CREATE TABLE Persona(persona INT AUTO_INCREMENT NOT NULL PRIMARY KEY, profesion VARCHAR(100), fecha_nac DATE, name VARCHAR(100), wikipedia BOOL, wikipedia_url VARCHAR(250))") #Persona PK
 
-nombreBase = "noticias_region_aysen"
-noticieros= [
-  "EPD_noticias","Viento_patagon","El_divisadero","Diario_aysen","El_chalenko","Radio_santa_maria",
-  "Diario_aysen_opina","Tehuelche_noticias","Canal_sur_patagonia","Risco_aysen","Radio_las_nieves_FM"
-]
-creaData(nombreBase)
-for i in noticieros:
-  creacion(nombreBase,i)
+    cur.execute("CREATE TABLE Score (puntaje INT PRIMARY KEY, persona INT REFERENCES Persona(persona), fechaPuntaje DATE, value INT)") #Puntaje FK (Persona)
 
-cur.execute("SHOW TABLES")
-for row in cur:
-  print(row)
+    cur.execute("CREATE TABLE Medio(url_medio VARCHAR(250) NOT NULL PRIMARY KEY, name VARCHAR(100), creation_date DATE, region INT, country VARCHAR(50), language VARCHAR(50))") #Medios PK    
 
+    cur.execute("CREATE TABLE Noticias(noticia_url VARCHAR(250) NOT NULL PRIMARY KEY, author_noticia VARCHAR(100), title_noticia VARCHAR(100), text_noticia TEXT, fecha_noticia DATE, url_medio VARCHAR(250) REFERENCES Medio(url_medio))") #Noticia FK (Medios)
+
+    cur.execute("CREATE TABLE Dueno(dueno VARCHAR(250) NOT NULL PRIMARY KEY, name VARCHAR(100), is_person BOOL)") #Dueño PK
+
+    cur.execute("CREATE TABLE Menciones(mencionar VARCHAR(200) PRIMARY KEY, noticia VARCHAR(250) NOT NULL, persona INT NOT NULL, FOREIGN KEY(noticia) REFERENCES Noticias(noticia_url), FOREIGN KEY(persona) REFERENCES Persona(persona))") #Menciones FK (Noticia y Persona)
+
+    cur.execute("CREATE TABLE Posee(posee VARCHAR(250) PRIMARY KEY, dueno VARCHAR(250) NOT NULL, url_medio VARCHAR(250) NOT NULL, fecha_inicio DATE, fecha_fin DATE, FOREIGN KEY(dueno) REFERENCES Dueno(dueno), FOREIGN KEY(url_medio) REFERENCES Medio(url_medio))") #Posee FK (Medios y Dueños)
 
 
 
+    
 
+createDatabase()
+createTables()
+#cur.execute("DRPO DATABASE Sophia3")
+print("Creado con éxito")
+conn.commit() 
+conn.close()
